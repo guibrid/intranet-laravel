ef58ec3c0ad377eb58bc6cc704985219c7e574b9
Guillaume Briard <gbriard@lillebonne.hdv>
2020-09-15 15:16:31 +0200

    Initial Commit

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!-- Stored in resources/views/layouts/app.blade.php -->
+
+<html>
+    <head>
+        <title>App Name</title>
+    </head>
+    <body>
+        
+        @yield('content')
+       
+    </body>
+    <script type="text/javascript" src="js/app.js"></script>
+</html>
